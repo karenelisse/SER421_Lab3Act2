9e1bac9e92dd81e866bb88ecbaeb94fe799486bf
kzaunsch <kzaunsch@asu.edu>
2017-11-06 12:24:29 +1100

    Minor Updates

--- lab3act2.js
+++ lab3act2.js
@@ -2,9 +2,10 @@
 // load the things we need
 var express = require('express');
 var app = express();
-var cookieParser = require('cookie-parser');
-xmlparser = require('express-xml-bodyparser');
-app.use(express.json());
+var ejs = require('ejs');
+
+
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -24,19 +25,6 @@ app.get('/logger', function(req, res) {
 // about page 
 app.get('/articles', function(req, res) {
     res.render('pages/articles');
-});
-
-//cookies
-app.use(cookieParser());
-
-//Set Cookie and Set Expir Date
-app.get('/cookie',function(req, res){
-     res.cookie(cookie_name , 'cookie_value', {expire : new Date() + 9999}).send('Cookie is set');
-});
-//deletes cookies
-app.get('/clearcookie', function(req,res){
-     clearCookie('cookie_name');
-     res.send('Cookie deleted');
 });
 
 
