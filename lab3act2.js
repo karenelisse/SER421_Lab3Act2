8e17258432dc654a9939f15a05fa1580ccac56be
kzaunsch <kzaunsch@asu.edu>
2017-11-07 23:10:53 +1100

    Added Express-Session
    Started the additional Express Session

--- lab3act2.js
+++ lab3act2.js
@@ -1,6 +1,7 @@
 // lab3act2.js
 // load the things we need
 var express = require('express');
+var session = require('express-session');
 var app = express();
 
 var cookieParser = require('cookie-parser');
@@ -223,6 +224,26 @@ app.post('/logger', function(req, res) {
 app.get('/articles', function(req, res) {
     res.render('pages/articles');
 });
+
+
+//express sessions
+
+// Use the session middleware
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+ 
+// Access the session as req.session
+app.get('/', function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+})
 
 //cookies
 app.use(cookieParser());
